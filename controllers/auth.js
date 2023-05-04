c37d289db42af8689e50a68cd7475834fc694edc
sup1214 <sup1214@naver.com>
2023-05-04 14:30:04 +0900

    nodebird complete basic

---
+++ controllers/auth.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const User = require("../models/user");
+
+exports.join = async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect("/join?error=exist");
+    }
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
